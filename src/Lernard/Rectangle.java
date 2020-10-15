bb504590674c88f1f8b2f512bd36aec008912b60
LernardDranrel <rezusfactor98@gmail.com>
2020-10-15 10:28:21 +0300

    Initial commit

---
+++ src/Lernard/Rectangle.java
@@ -0,0 +1,29 @@
+package Lernard;
+
+public class Rectangle extends Shape {
+    double width;
+    double length;
+    public Rectangle(double width, double length, String color, boolean filled)
+    {
+        super(color, filled);
+        this.length=length;
+        this.width=width;
+    }
+
+    public double getArea()
+    {
+        return (width*length);
+    }
+    public double getPerimeter()
+    {
+        return (2*(width+length));
+    }
+    public String toString()
+    {
+        return (width+" "+length+" "+getArea()+" "+getPerimeter());
+    }
+    public double getLength()
+    {
+        return length;
+    }
+}
